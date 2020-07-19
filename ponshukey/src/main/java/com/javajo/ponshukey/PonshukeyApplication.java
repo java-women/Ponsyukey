3d2a45bf0a5a3c9e790c8eb675718efdac486f3d
chezyn <9652497+chezyn@users.noreply.github.com>
2020-07-19 17:16:52 +0900

    spring bootプロジェクト追加

---
+++ ponshukey/src/main/java/com/javajo/ponshukey/PonshukeyApplication.java
@@ -0,0 +1,13 @@
+package com.javajo.ponshukey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonshukeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonshukeyApplication.class, args);
+	}
+
+}
---
+++ ponshukey/src/test/java/com/javajo/ponshukey/PonshukeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajo.ponshukey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonshukeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
