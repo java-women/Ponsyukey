8ee58673527ccf4fb0255d25f4106315a89ea10f
maaya <huki.j37@gmail.com>
2021-04-18 15:46:36 +0900

    TDDもどきのためのテストコード書き始めました
    failします

---
+++ src/main/java/javajo/ponsyukey/PonsyukeyApplication.java
@@ -0,0 +1,11 @@
+package javajo.ponsyukey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonsyukeyApplication {
+    public static void main(String... args){
+        SpringApplication.run(PonsyukeyApplication.class, args);
+    }
+}
---
+++ src/test/java/javajo/ponsyukey/controller/SakeApiControllerTest.java
@@ -0,0 +1,36 @@
+package javajo.ponsyukey.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SakeApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void 酒情報取得_sakeIdが正しい値の場合httpステータスコード200が返却されること() throws Exception {
+        this.mockMvc.perform(get("/sake/1")).andDo(print()).andExpect(status().isOk());
+    }
+
+
+}
