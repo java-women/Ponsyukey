c51bfc213cb5bff0cd7269d17ff0da083e3d60c6
maaya ishida <huki.j37@gmail.com>
2021-07-18 16:11:14 +0900

    Update db/ddl/01_create_saketomo_tables.sql
    Co-authored-by: chiaki <80730827+chi-cmd@users.noreply.github.com>

--- db/ddl/01_create_saketomo_tables.sql
+++ db/ddl/01_create_saketomo_tables.sql
@@ -14,3 +14,18 @@ CREATE TABLE saketomo
 );
 
 ALTER TABLE saketomo ADD CONSTRAINT pk_saketomo_id PRIMARY KEY CLUSTERED (id);
+
+EXEC sys.sp_addextendedproperty  
+     @name=N'MS_Description',@value=N'UUIDはアプリで生成する前提です。'       
+    ,@level0type=N'SCHEMA',@level0name=N'ponsyukey'  ,@level1type=N'TABLE' ,@level1name=N'saketomo' 
+    ,@level2type=N'COLUMN',@level2name=N'id'  
+    
+EXEC sys.sp_addextendedproperty  
+     @name=N'MS_Description',@value=N'emailアドレスは暗号化されて入ってきます。'       
+    ,@level0type=N'SCHEMA',@level0name=N'ponsyukey'  ,@level1type=N'TABLE' ,@level1name=N'saketomo' 
+    ,@level2type=N'COLUMN',@level2name=N'email'  
+    
+EXEC sys.sp_addextendedproperty  
+     @name=N'MS_Description',@value=N'パスワードカラムは暗号化されて入ってきます。'       
+    ,@level0type=N'SCHEMA',@level0name=N'ponsyukey'  ,@level1type=N'TABLE' ,@level1name=N'saketomo' 
+    ,@level2type=N'COLUMN',@level2name=N'email'  
