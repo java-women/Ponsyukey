a1679368a5861ed48191211571d861e21aaaa1e1
maaya ishida <huki.j37@gmail.com>
2021-07-18 16:29:59 +0900

    Update db/data/add_regions.sql
    Co-authored-by: Ellie-02 <65482122+Ellie-02@users.noreply.github.com>

--- db/data/add_regions.sql
+++ db/data/add_regions.sql
@@ -1,5 +1,7 @@
 -- countryの初期データをInsertする
 
 -- prefectureの初期データをInsertする
-
+BULK INSERT prefecture
+FROM 'db/data/bulk_insert_prefecture.csv'
+WITH ( FIELDTERMINATOR =',' );
 -- regionsの初期データをInsertする
