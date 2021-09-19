75ca8ec4ea11a0b97b8b5af82abdb12143cfa6bb
ihcomega56 <ihcomega56@gmail.com>
2021-09-19 16:15:54 +0900

    環境を作るためのシェルスクリプトを追加した

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+echo Dockerを立ち上げる
+docker-compose up
---
+++ setup/insert-sake.sql
@@ -0,0 +1,7 @@
+use ponsyukey;
+insert into country values(81, '日本');
+insert into prefecture values(1, '北海道');
+insert into region values(1, 81, 1);
+insert into brewery values('30149f16-ec9c-58a2-f7f5-78b0620d0dc7', 'じゃばじょ酒屋', '1');
+insert into sake values('29892458-f103-89d5-a724-72f789e16e3b', 'ナイスお酒', 'https://image1.javajo.com', '30149f16-ec9c-58a2-f7f5-78b0620d0dc7', 10, 0.5, '純米大吟醸', 'おいしいお酒です！');
+go
