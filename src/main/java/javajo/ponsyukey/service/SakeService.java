b4a1b32db7913c899e85458836fb4033c5473bc0
maaya <huki.j37@gmail.com>
2021-12-19 16:17:56 +0900

    [WIP] 20回イベント中

--- src/main/java/javajo/ponsyukey/service/SakeService.java
+++ src/main/java/javajo/ponsyukey/service/SakeService.java
@@ -3,6 +3,7 @@ package javajo.ponsyukey.service;
 import javajo.ponsyukey.database.dao.SakeDao;
 import javajo.ponsyukey.model.Sake;
 import javajo.ponsyukey.model.SakeResponse;
+import javajo.ponsyukey.repository.SakeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +12,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SakeService {
-    private final SakeDao sakeDao;
+    private final SakeRepository sakeRepository;
 
     @Autowired
-    public SakeService(SakeDao sakeDao) {
-        this.sakeDao = sakeDao;
+    public SakeService(SakeRepository sakeRepository) {
+        this.sakeRepository = sakeRepository;
     }
 
+    /**
+     * 指定した酒IDに紐づくお酒詳細情報を取得する
+     * @param sakeId 酒ID
+     * @return お酒詳細情報一式
+     */
     public SakeResponse getSakeResponse(String sakeId) {
-
+        //酒IDに紐づく酒情報を取得する
+        Sake sake = sakeRepository.getSake(sakeId);
 
         return new SakeResponse()
-                //.resultCode()
-                //.sake()
+//                .resultCode()
+//                .sake(sake)
                 ;
     }
 }
