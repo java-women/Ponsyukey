58f5da0c99797bce1cb07c8795c2e2a1c17be94b
maaya <huki.j37@gmail.com>
2021-12-19 17:01:37 +0900

    APIレスポンス汎用Exception作成

--- src/main/java/javajo/ponsyukey/service/SakeService.java
+++ src/main/java/javajo/ponsyukey/service/SakeService.java
@@ -25,8 +25,6 @@ public class SakeService {
      * @return お酒詳細情報一式
      */
     public SakeResponse getSakeResponse(String sakeId) {
-        //TODO バリデーションエラーの場合はerrorオブジェクトを返却する
-
         //酒IDに紐づく酒情報を取得する
         Sake sake = sakeRepository.getSake(sakeId);
 
